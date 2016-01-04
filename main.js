82eafeec8222137169152bf66d8ed98c0e0481b7
kolbeypruitt <kolbeypruitt@gmail.com>
2016-01-04 13:22:04 -0700

    initial commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1200,800,Phaser.AUTO,'gameDiv');
+var game = new Phaser.Game(800,600,Phaser.AUTO,'gameDiv');
 
 var starfield;
 
@@ -21,16 +21,20 @@ var winText;
 var mainState = {
     preload: function () {
         game.load.image('starfield', 'assets/starfield.png');
-        game.load.image('player', 'assets/spaceship.png');
+        game.load.spritesheet('player_down', 'assets/marvin/walk_down.png', 64, 60, 8);
+        game.load.spritesheet('player_up', 'assets/marvin/walk_up.png', 64, 60, 8);
+        game.load.spritesheet('player_left', 'assets/marvin/walk_left.png', 64, 60, 8);
+        game.load.spritesheet('player_right', 'assets/marvin/walk_right.png', 64, 60, 8);
         game.load.image('bullet', 'assets/bullet.png');
         game.load.image('enemy', 'assets/pikachu.png');
     },
 
     create: function () {
-        starfield = game.add.tileSprite(0,0,1200,800,'starfield');
-        backgroundV = 30;
+        starfield = game.add.tileSprite(0,0,800,600,'starfield');
+        backgroundV = 3;
 
-        player = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'player');
+        player = game.add.sprite(game.world.centerX, game.world.centerY + 200, 'player_down');
+
         game.physics.enable(player,Phaser.Physics.ARCADE);
 
         cursors = game.input.keyboard.createCursorKeys();
@@ -52,26 +56,39 @@ var mainState = {
 
         createEnemies();
 
-
-        scoreText = game.add.text(0,550,'Score:',{font: '32px Arial',fill: '#fff'});
-        winText = game.add.text(game.world.centerX,game.world.centerX,'You Win!',{font: '32px Arial',fill: '#fff'});
+        scoreText = game.add.text(10,550,'Score:',{font: '32px Arial',fill: '#fff'});
+        winText = game.add.text(game.world.centerX,game.world.centerY,'You Win!',{font: '32px Arial',fill: '#fff'});
         winText.visible = false;
     },
 
     update: function () {
 
-        game.physics.arcade.overlap(bullets,enemies,collisionHandler,null,this);
+        game.physics.arcade.overlap(bullets,enemies,enemyKill,null,this);
+        game.physics.arcade.overlap(player,enemies,playerKill,null,this);
 
         player.body.velocity.x = 0;
+        player.body.velocity.y = 0;
 
         starfield.tilePosition.y += backgroundV;
 
         if(cursors.left.isDown) {
+            changeSprite("left");
             player.body.velocity.x = -600;
         }
 
         if(cursors.right.isDown) {
+            changeSprite("right");
             player.body.velocity.x = 600;
+        }
+
+        if(cursors.up.isDown) {
+            changeSprite("up");
+            player.body.velocity.y = -600;
+        }
+
+        if(cursors.down.isDown) {
+            changeSprite("down");
+            player.body.velocity.y = 600;
         }
 
         if(fireButton.isDown) {
@@ -117,14 +134,39 @@ function createEnemies() {
 };
 
 function descend() {
-    enemies.y += 10;
+    enemies.y += 50;
 };
 
-function collisionHandler(bullet,enemy) {
+function enemyKill(bullet,enemy) {
     bullet.kill();
     enemy.kill();
 
+
     score += 100;
+}
+
+function playerKill(player,enemy) {
+    player.kill();
+}
+
+function changeSprite(sprite) {
+    if (sprite==="up") {
+        player.loadTexture('player_up', 0);
+    }
+    if (sprite==="down") {
+        player.loadTexture('player_down', 0);
+    }
+    if (sprite==="left") {
+        player.loadTexture('player_left', 0);
+    }
+    if (sprite==="right") {
+        player.loadTexture('player_right', 0);
+    }
+
+    player.animations.add('walk');
+
+    player.animations.play('walk', 7, true);
+
 }
 
 game.state.add('mainState', mainState);
