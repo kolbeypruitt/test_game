f95b7b5efd552ab3f9ac4d3dc821d4981bbae361
kolbeypruitt <kolbeypruitt@gmail.com>
2016-01-05 14:34:29 -0700

    cleanup
    cleaned code up and removed redundancy

--- Tank.js
+++ Tank.js
@@ -28,13 +28,17 @@ var Tank = function(index, game, player) {
     this.bullets.setAll('outOfBoundsKill', true);
     this.bullets.setAll('checkWorldBounds', true);
 
-
     this.currentSpeed = 0;
     this.fireRate = 500;
     this.nextFire = 0;
     this.alive = true;
 
     this.tank = game.add.sprite(x, y, 'walking', 27);
+
+    this.tank.animations.add('walk_left', [0,1,2,3,4,5,6,7,8], 60, false, true);
+    this.tank.animations.add('walk_right', [9,10,11,12,13,14,15,16,17], 60, false, true);
+    this.tank.animations.add('walk_up', [18,19,20,21,22,23,24,25,26], 60, false, true);
+    this.tank.animations.add('walk_down', [27,28,29,30,31,32,33,34,35], 60, false, true);
 
     this.tank.anchor.set(0.5);
 
@@ -80,33 +84,18 @@ Tank.prototype.update = function() {
 
     if (this.cursor.left) {
         this.tank.x += -1;
-        // this.tank.angle -= 1;
+        this.tank.animations.play('walk_left', 8, false, false);
     } else if (this.cursor.right) {
-        // this.tank.body.velocity.x = 150;
+        this.tank.animations.play('walk_right', 8, false, false);
         this.tank.x += 1;
-        // this.tank.angle += 1;
     }
     if (this.cursor.up) {
+        this.tank.animations.play('walk_up', 8, false, false);
         this.tank.y += -1;
-        //  The speed we'll travel at
-        // this.currentSpeed = 100;
     }
     if (this.cursor.down) {
+        this.tank.animations.play('walk_down', 8, false, false);
         this.tank.y += 1;
-        //  The speed we'll travel at
-        // this.currentSpeed = 200;
-    }
-    // else {
-    //     if (this.currentSpeed > 0) {
-    //         this.currentSpeed -= 4;
-    //     }
-    // }
-
-
-    if (this.currentSpeed > 0) {
-        game.physics.arcade.velocityFromRotation(this.tank.rotation, this.currentSpeed, this.tank.body.velocity);
-    } else {
-        game.physics.arcade.velocityFromRotation(this.tank.rotation, 0, this.tank.body.velocity);
     }
 
 };
--- main.js
+++ main.js
@@ -53,7 +53,7 @@ var eurecaClientSetup = function() {
   }
 
   eurecaClient.exports.updateState = function(id, state) {
-    console.log('updating state', id, state)
+    // console.log('updating state', id, state)
       if (currentUsers[id])  {
           currentUsers[id].cursor = state;
           currentUsers[id].tank.x = state.x;
@@ -89,6 +89,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', {
         game.load.image('bullet', 'assets/bullet.png');
         game.load.image('enemy', 'assets/pikachu.png');
         game.load.image('grass', 'assets/grass.png');
+        game.load.image('logo', 'assets/game_logo.png');
     }
 
     // function create() {
@@ -165,10 +166,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', {
 
     tank.bringToTop();
         
-    // logo = game.add.sprite(0, 200, 'logo');
-    // logo.fixedToCamera = true;
+    logo = game.add.sprite(150, 200, 'logo');
+    logo.fixedToCamera = true;
 
-    // game.input.onDown.add(removeLogo, this);
+    game.input.onDown.add(removeLogo, this);
 
     game.camera.follow(tank);
     game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
@@ -176,66 +177,24 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', {
 
     cursors = game.input.keyboard.createCursorKeys();
     
-    // setTimeout(removeLogo, 1000);
-    
+    setTimeout(removeLogo, 2000);
+
+
+
+
+
+
+
+    scoreText = game.add.text(10,550,'Score:',{font: '32px Arial',fill: '#fff'});
+    scoreText.fixedToCamera = true;
+    winText = game.add.text(game.world.centerX,game.world.centerY,'You Win!',{font: '32px Arial',fill: '#fff'});
+    winText.visible = false;
 }
 
-// function removeLogo () {
-//     game.input.onDown.remove(removeLogo, this);
-//     logo.kill();
-// }
-
-    // function update () {
-    //   //do not update if client not ready
-    //   if (!ready) return;
-
-    //     game.physics.arcade.overlap(bullets,enemies,enemyKill,null,this);
-    //     game.physics.arcade.overlap(player,enemies,playerKill,null,this);
-
-    //     // player.body.velocity.x = 0;
-    //     // player.body.velocity.y = 0;
-
-    //     // player.animations.stop();
-
-    //     player.input.left = cursors.left.isDown;
-    //     player.input.right = cursors.right.isDown;
-    //     player.input.up = cursors.up.isDown;
-    //     player.input.tx = game.input.x+ game.camera.x;
-    //     player.input.ty = game.input.y+ game.camera.y;
-
-    //     // if (cursors.left.isDown){
-    //     //     player.body.velocity.x = -150;
-    //     //     player.animations.play('walk_left', 8, false, false);
-    //     // }
-    //     // if(cursors.right.isDown) {
-    //     //     player.animations.play('walk_right', 8, false, false);
-    //     //     player.body.velocity.x = 150;
-    //     // }
-    //     // if(cursors.up.isDown) {
-    //     //     player.animations.play('walk_up', 8, false, false);
-    //     //     player.body.velocity.y = -150;
-    //     // }
-    //     // if(cursors.down.isDown) {
-    //     //     player.animations.play('walk_down', 8, false, false);
-    //     //     player.body.velocity.y = 150;
-    //     // }
-    //     // player.animations.play('run', 7, false, false);
-    //     // if(fireButton.isDown) {
-    //     //     fireBullet();
-    //     // }
-
-
-    //     // scoreText.text = 'Score: ' + score;
-
-
-    //     if(score == 4000) {
-    //         winText.visible = true;
-    //         scoreText.visible = false;
-    //     }
-
-    //     land.tilePosition.x = -game.camera.x;
-    //     land.tilePosition.y = -game.camera.y;
-    // }
+function removeLogo () {
+    game.input.onDown.remove(removeLogo, this);
+    logo.kill();
+}
 
     function update () {
         //do not update if client not ready
@@ -245,18 +204,13 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', {
         player.input.right = cursors.right.isDown;
         player.input.up = cursors.up.isDown;
         player.input.down = cursors.down.isDown;
-        // player.input.fire = game.input.activePointer.isDown;
+        player.input.fire = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).isDown;
         player.input.tx = game.input.x+ game.camera.x;
         player.input.ty = game.input.y+ game.camera.y;
         
-        
-        
-        // turret.rotation = game.physics.arcade.angleToPointer(turret);   
         land.tilePosition.x = -game.camera.x;
         land.tilePosition.y = -game.camera.y;
 
-            
-        
         for (var i in currentUsers)
         {
             if (!currentUsers[i]) continue;
@@ -279,56 +233,15 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', {
                 }           
             }
         }
-    }
 
-function fireBullet() {
-    if(game.time.now > bulletTime) {
-        bullet = bullets.getFirstExists(false);
 
-        if (bullet) {
-            bullet.reset(player.x + 32, player.y);
-            bullet.body.velocity.y = -400;
-            bulletTime = game.time.now + 200;
+        scoreText.text = 'Score: ' + score;
+
+        if(score == 4000) {
+            winText.visible = true;
+            scoreText.visible = false;
         }
+
     }
-}
-
-function createEnemies() {
-    for (var y = 0; y < 4; y++) {
-        for (var x = 0; x < 10; x++) {
-            var enemy = enemies.create(x*48,y*50, 'enemy');
-            enemy.anchor.setTo(0.5,0.5);
-        }
-    }
-
-    enemies.x = 100;
-    enemies.y = 50;
-
-
-    var tween = game.add.tween(enemies).to({x:200},2000,Phaser.Easing.Linear.None,true,0,1000,true);
-
-    tween.onLoop.add(descend,this);
-};
-
-function descend() {
-    enemies.y += 20;
-};
-
-function enemyKill(bullet,enemy) {
-    bullet.kill();
-    enemy.kill();
-
-
-    score += 100;
-}
-
-function playerKill(player,enemy) {
-    player.kill();
-}
-
 
 function render() {}
-
-// game.state.add('mainState', mainState);
-
-// game.state.start('mainState');
